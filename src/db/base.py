c8404006f7cb2e179098f1c9d9b3e136cbb05b05
lordavadon <lordavadon22@gmail.com>
2022-02-19 20:02:32 +0200

    Updated model Add alembic and migrations

---
+++ src/db/base.py
@@ -0,0 +1,1 @@
+from src.shop import models
--- src/shop/models.py
+++ src/shop/models.py
@@ -1,6 +1,9 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, Text, DECIMAL, Boolean, DateTime
+from slugify import slugify
+from sqlalchemy import Column, Integer, String, Text, DECIMAL, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import URLType
 
 from src.db.database import Base
 
@@ -10,6 +13,14 @@ class Category(Base):
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String)
+    slug = Column(String, unique=True)
+
+    products = relationship('Product', back_populates='category')
+
+    def __init__(self, *args, **kwargs):
+        if 'slug' not in kwargs:
+            kwargs['slug'] = slugify(kwargs.get('slug'))
+        super().__init__(*args, **kwargs)
 
 
 class Product(Base):
@@ -18,7 +29,17 @@ class Product(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String)
     description = Column(Text)
+    url = Column(URLType)
     price = Column(DECIMAL(scale=2))
     available = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.datetime.now)
+    slug = Column(String, unique=True)
+
+    def __init__(self, *args, **kwargs):
+        if 'slug'not in kwargs:
+            kwargs['slug'] = slugify(kwargs.get('slug'))
+        super().__init__(*args, **kwargs)
+
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship('Category', back_populates='products')
